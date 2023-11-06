39ed185e6398af411cac48f77220dbd7050d654d
aniketgupta01 <aniketg2831@gmail.com>
2023-11-06 13:18:14 +0530

    scaled up

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 let details = document.getElementById('add-details')
       details.addEventListener('submit',myFunction);
+let customers = document.getElementById('customers');      
 
       function myFunction(e){
         e.preventDefault();
         let myObj = {
           name:fname.value,
-          email:email.value
+          email:email.value,
+          number:number.value
         }
         let myObj_serial = JSON.stringify(myObj);
-        localStorage.setItem(fname.value,email.value)
-        localStorage.setItem('myObj',myObj_serial)
+        localStorage.setItem(email.value,myObj_serial)
+
+        let li = document.createElement('li');
+        li.appendChild(document.createTextNode(`Name : ${fname.value} Email : ${email.value} Number : ${number.value}`));
+
+        customers.appendChild(li);
 
         
       }
