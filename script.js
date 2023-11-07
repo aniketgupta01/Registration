8bc76418283acc57717c334e631374b032c92b16
aniketgupta01 <aniketg2831@gmail.com>
2023-11-07 16:22:41 +0530

    added edit feature

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let nameInput = document.getElementById('fname');
+let emailInput = document.getElementById('email');
+let numberInput = document.getElementById('number');
 let details = document.getElementById('add-details')
       details.addEventListener('submit',myFunction);
 let customers = document.getElementById('customers');      
@@ -13,27 +16,55 @@ let customers = document.getElementById('customers');
         localStorage.setItem(email.value,myObj_serial)
 
         let li = document.createElement('li');
-        li.appendChild(document.createTextNode(`Name : ${fname.value} Email : ${email.value} Number : ${number.value}`));
+        li.appendChild(document.createTextNode(`${fname.value},${email.value},${number.value}`));
 
+        //Edit button
+        let editBtn = document.createElement('button');
+        editBtn.className='editbtn';
+        editBtn.id='edit';
+        editBtn.textContent='Edit';
+        
+        //Delete Button
         let deleteBtn = document.createElement('button');
         deleteBtn.className='deletebtn';
         deleteBtn.id='delete';
         deleteBtn.textContent='Remove'
 
+
         li.appendChild(deleteBtn);
+        li.appendChild(editBtn);
         customers.appendChild(li);
+        fname.value=""
+        email.value=""
+        number.value=""
 
         
       }
 
       customers.addEventListener('click', deleteItem);
+      customers.addEventListener('click',editItem);
 
       function deleteItem(e){
         if(e.target.classList.contains('deletebtn')){
           let item = e.target.parentElement;
           customers.removeChild(item);
 
-          localStorage.removeItem(email.value)
+          let arr = e.target.parentElement.innerText.split(',');
+          localStorage.removeItem(arr[1]);
 
+
+        }
+      }
+      function editItem(e){
+        if(e.target.classList.contains('editbtn')){
+          let item = e.target.parentElement;
+          customers.removeChild(item);
+
+          let arr = e.target.parentElement.firstChild.textContent.split(',');
+          localStorage.removeItem(arr[1]);
+
+          nameInput.value=arr[0];
+          emailInput.value=arr[1];
+          numberInput.value=arr[2];
         }
       }
