32a2dbd7ef971df72d62669074aa2a8e5dadcb12
aniketgupta01 <aniketg2831@gmail.com>
2023-11-26 00:38:22 +0530

    axios get

--- script.js
+++ script.js
@@ -1,77 +1,86 @@
-let nameInput = document.getElementById('fname');
-let emailInput = document.getElementById('email');
-let numberInput = document.getElementById('number');
-let details = document.getElementById('add-details')
-details.addEventListener('submit',myFunction);
-let customers = document.getElementById('customers');      
-
-      function myFunction(e){
-        e.preventDefault();
-        let myObj = {
-          name:nameInput.value,
-          email:emailInput.value,
-          number:numberInput.value
-        }
-        axios.post('https://crudcrud.com/api/bda3a984e7124188b6a66581fb47e0ff/appointmentData',myObj)
-        .then((res) => {
-          console.log(res.data)
-        })
-        .catch((err)=>{
-          console.log(err)
-        });
-        
-        let li = document.createElement('li');
-        li.appendChild(document.createTextNode(`${nameInput.value},${emailInput.value},${numberInput.value}`));
-      
-
-        //Edit button
-        let editBtn = document.createElement('button');
-        editBtn.className='editbtn';
-        editBtn.id='edit';
-        editBtn.textContent='Edit';
-        
-        //Delete Button
-        let deleteBtn = document.createElement('button');
-        deleteBtn.className='deletebtn';
-        deleteBtn.id='delete';
-        deleteBtn.textContent='Remove'
-
-
-        li.appendChild(deleteBtn);
-        li.appendChild(editBtn);
-        customers.appendChild(li);
-        nameInput.value=""
-        emailInput.value=""
-        numberInput.value=""
-      
-
-        
+const myForm = document.querySelector("#my-form");
+const nameInput = document.querySelector("#fname");
+const emailInput = document.querySelector("#email");
+const phoneInput = document.querySelector("#number");
+// const msg = document.querySelector(".msg");
+const userList = document.querySelector("#customers");
+myForm.addEventListener("submit", onSubmit);
+function onSubmit(e) {
+  e.preventDefault();
+  let myoj = {
+    name: nameInput.value,
+    email: emailInput.value,
+    phone: phoneInput.value,
+  };
+  
+  axios
+    .post(
+      "https://crudcrud.com/api/1e984d0ffa4944ed8c3374036bb241a2/appointmentdata",
+      myoj
+    )
+    .then((res) => showonscreen(res.data))
+    .catch((err) => console.log(err));
+  nameInput.value = "";
+  emailInput.value = "";
+  phoneInput.value = "";
+}
+window.addEventListener("DOMContentLoaded", () => {
+  axios
+    .get(
+      "https://crudcrud.com/api/1e984d0ffa4944ed8c3374036bb241a2/appointmentdata"
+    )
+    .then((res) => {
+      for (var i = 0; i < res.data.length; i++) {
+        showonscreen(res.data[i]);
       }
-
-      customers.addEventListener('click', deleteItem);
-      customers.addEventListener('click',editItem);
-
-      function deleteItem(e){
-        if(e.target.classList.contains('deletebtn')){
-          let item = e.target.parentElement;
-          customers.removeChild(item);
-
-          let arr = e.target.parentElement.innerText.split(',');
-          localStorage.removeItem(arr[1]);
-
-
-        }
-      }
-      function editItem(e){
-        if(e.target.classList.contains('editbtn')){
-          let item = e.target.parentElement;
-          customers.removeChild(item);
-
-          let arr = e.target.parentElement.firstChild.textContent.split(',');
-          localStorage.removeItem(arr[1]);
-
-          nameInput.value=arr[0];
-          emailInput.value=arr[1];
-          numberInput.value=arr[2];
-        }
-      }
+    })
+    .catch((err) => console.log(err));
+});
+function showonscreen(obj) {
+  let name = obj.name;
+  let email = obj.email;
+  let phone = obj.phone;
+  let userid = obj._id;
+  const childHTML = `<li id=${userid}>${name} ${email} ${phone} 
+      <button class="deletebtn" onclick=deletefn('${userid}')>Delete</button>
+      <button class="editbtn" onclick=editfn('${userid}')>Edit</button></li>`;
+  userList.innerHTML = userList.innerHTML + childHTML;
+}
+function deletefn(userid) {
+//   console.log(userid);
+//   axios
+//     .delete(
+//       `https://crudcrud.com/api/1e984d0ffa4944ed8c3374036bb241a2/appointmentdata/${userid}`
+//     )
+//     .then((response) => {
+//       removeuserfromscreen(userid);
+//     })
+//     .catch((err) => console.log(err));
+// }
+// function removeuserfromscreen(userid) {
+//   const childnodedelt = document.getElementById(userid);
+//   if (childnodedelt) {
+//     userList.removeChild(childnodedelt);
+//   }
+}
+function editfn(userid) {
+  // axios
+  //   .get(
+  //     `https://crudcrud.com/api/1e984d0ffa4944ed8c3374036bb241a2/appointmentdata/${userid}`
+  //   )
+  //   .then((res) => {
+  //     nameInput.value = res.data.name;
+  //     emailInput.value = res.data.email;
+  //     phoneInput.value = res.data.phone;
+  //   })
+  //   .catch((err) => console.log(err));
+  // axios
+  //   .delete(
+  //     `https://crudcrud.com/api/1e984d0ffa4944ed8c3374036bb241a2/appointmentdata/${userid}`
+  //   )
+  //   .then((response) => {
+  //     removeuserfromscreen(userid);
+  //   })
+  //   .catch((err) => console.log(err));
+  // removeuserfromscreen(userid);
+}
